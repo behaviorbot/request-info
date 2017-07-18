cc03cb35b711ddd92171d056eb3037a67eacce7c
hiimbex <hiimbex@github.com>
2017-07-18 10:44:22 -0700

    removed labels, renamed bodies -> config, added syntax shortcuts

--- index.js
+++ index.js
@@ -4,35 +4,29 @@ module.exports = robot => {
     robot.on('pull_request.opened', receive);
     robot.on('issues.opened', receive);
     async function receive(context) {
-        let title, body, number, bodies, badTitle;
+        let title, body, config, badTitle;
         if (context.payload.pull_request) {
-            ({ title, body, number } = context.payload.pull_request);
+            ({ title, body } = context.payload.pull_request);
         } else {
-            ({ title, body, number } = context.payload.issue);
+            ({ title, body } = context.payload.issue);
         }
 
         try {
             const options = context.repo({path: '.github/config.yml'});
             const response = await context.github.repos.getContent(options);
-            bodies = yaml.load(Buffer.from(response.data.content, 'base64').toString()) || {};
+            config = yaml.load(Buffer.from(response.data.content, 'base64').toString()) || {};
         } catch (err) {
             if (err.code !== 404) throw err;
         }
 
-        if (bodies) {
-            if (bodies.requestInfoDefaultTitles.includes(title.toLowerCase())) badTitle = true;
+        if (config) {
+            if (config.requestInfoDefaultTitles.includes(title.toLowerCase())) badTitle = true;
         }
         if (!body || badTitle) {
-            context.github.issues.createComment(context.issue({body: bodies.requestInfoReplyComment}));
-            if (bodies.requestInfoLabelToAdd) {
+            context.github.issues.createComment(context.issue({body: config.requestInfoReplyComment}));
+            if (config.requestInfoLabelToAdd) {
                 // Add label if there is one listed in the yaml file
-                const repo = context.payload.repository;
-                context.github.issues.addLabels({
-                    owner: repo.owner.login,
-                    repo: repo.name,
-                    number,
-                    labels: [bodies.requestInfoLabelToAdd]
-                });
+                context.github.issues.addLabels(context.issue({labels: [config.requestInfoLabelToAdd]}));
             }
         }
     }
