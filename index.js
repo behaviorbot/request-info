7d7ed2fa84da4a666bdcb63481f1892c018db374
Shelley Vohr <shelley.vohr@gmail.com>
2017-10-19 11:53:56 -0400

    add beginning infra for template matching

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const getComment = require('./lib/getComment')
 const defaultConfig = require('./lib/defaultConfig')
+const issueTemplateMatches = require('./lib/getIssueTemplate')
 
 module.exports = robot => {
   robot.on(['pull_request.opened', 'issues.opened'], receive)
   async function receive (context) {
     let title
     let body
+    let unmatchedBody
     let badTitle
     let user
 
@@ -30,13 +32,19 @@ module.exports = robot => {
         }
       }
 
+      if (config.matchIssueTemplate) {
+        if(!issueTemplateChecker.match(body)) {
+          unmatchedBody = true
+        }
+      }
+
       let notExcludedUser = true
       if (config.requestInfoUserstoExclude) {
         if (config.requestInfoUserstoExclude.includes(user.login)) {
           notExcludedUser = false
         }
       }
-      if ((!body || badTitle) && notExcludedUser) {
+      if ((!body || badTitle || unmatchedBody) && notExcludedUser) {
         const comment = getComment(config.requestInfoReplyComment, defaultConfig.requestInfoReplyComment)
         context.github.issues.createComment(context.issue({body: comment}))
 
